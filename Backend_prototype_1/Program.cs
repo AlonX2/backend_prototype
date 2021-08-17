ae2df5d318cd099ff2a3acebcfa3280b20f84fc3
Alon <alonat9@gmail.com>
2021-08-17 23:27:52 +0300

    fixed bullshit

--- Backend_prototype_1/Program.cs
+++ Backend_prototype_1/Program.cs
@@ -10,7 +10,7 @@ namespace Backend_prototype_1
             string pancake_factory_v2 = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";//pretty sure this is the pancake factory address.
             string wrapped_bnb_address = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
             string contract_pair_abi = File.ReadAllText("abi.json");
-            SwapRouter router = new SwapRouter(pancake_factory_v2, wrapped_bnb_address);
+            SwapRouter router = new SwapRouter(pancake_factory_v2, wrapped_bnb_address, contract_pair_abi);
         }
     }
 }
